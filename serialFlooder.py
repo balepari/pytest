e1576d3b8c35643ad72b59abfb9c9a34d4f583f9
balepari <paridebalestri@gmail.com>
2017-08-12 23:09:14 +0200

    009

--- serialFlooder.py
+++ serialFlooder.py
@@ -9,4 +9,6 @@ ser = serial.Serial('/dev/serial0', 115200, serial.EIGHTBITS, serial.PARITY_NONE
 while True:
     for i in range(33,126):
 
-        ser.write(ascii(i).encode('ascii'))
+        ser.write(str(i))
+    ser.write(b'\r\n')
+    
