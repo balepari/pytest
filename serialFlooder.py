d240f7527fe229336f21ae5cd68ddb9a78ee56f5
balepari <paridebalestri@gmail.com>
2017-08-12 22:54:08 +0200

    007

---
+++ serialFlooder.py
@@ -0,0 +1,11 @@
+## serialFlooder.py
+## This program is intended to write as many characters as possbile to serial port
+## like it's flooding it.
+
+import serial
+
+ser = serial.Serial('/dev/serial0', 115200, serial.EIGHTBITS, serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,xonxoff=True)
+
+while True:
+    for i in range(33,126):
+        ser.write(ascii(i))
