ccca6567a33affc46a3e47ab0df96c849870b307
balepari <paridebalestri@gmail.com>
2017-08-12 22:22:50 +0200

    004

--- serial01.py
+++ serial01.py
@@ -1,10 +1,17 @@
 import serial
+import logme
 
+logme.logme(name='serial', filename='trace.log', msg='Imposto la seriale...')
 ser = serial.Serial('/dev/serial0', 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, xonxoff=True)
 
 print(ser.name)
+
+logme.logme(name='serial', filename='trace.log', msg='Scrivo sulla seriale...')
 ser.write(b'Hello World!')
+logme.logme(name='serial', filename='trace.log', msg='Svuoto il buffer...')
 ser.flush()
-letto = ser.read(8)
-print(letto)
+#letto = ser.read(8)
+#print(letto)
+logme.logme(name='serial', filename='trace.log', msg='Chiudo la seriale...')
 ser.close()
+logme.logme(name='serial', filename='trace.log', msg='Termino...')
