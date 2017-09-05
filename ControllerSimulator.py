1863edb4077327622c8448755426190d93ad52e2
balepari <paridebalestri@gmail.com>
2017-09-05 20:55:43 +0200

    controllersimulator2
    funziona stand alone. non come generatore reiterabile.

--- ControllerSimulator.py
+++ ControllerSimulator.py
@@ -20,16 +20,15 @@
 import random
 import time
 
-
-min_dist_time = 150     # expressed in µs
-max_dist_time = 25000   # expressed in µs
-tShift = 0   # Time shift from start acquisition
+#def Controller (min_dist_time = 150, max_dist_time = 250, tShift = 0, tPause = 0.025, moving = False):
+min_dist_time = 150  # expressed in µs
+max_dist_time = 25000  # expressed in µs
+tShift = 0  # Time shift from start acquisition
 tPause = 0.025  # Time to wait for simulating ultrasonic sensor acquiring process
 
 moving = True  # Determines when we are simulating static object (False) or moving object (True)
 
-
-random.seed()   # initialize random number generator
+random.seed()  # initialize random number generator
 
 while True:
 
@@ -38,6 +37,7 @@ while True:
     tz = random.randint(min_dist_time, max_dist_time)
 
     print("%.5f\t%d\t%d\t%d" % (tShift, tx, ty, tz))
+    #       yield (tShift, tx, ty, tz)
     if moving:
         tPause = random.uniform(0.00150, 0.025)
         time.sleep(tPause)
@@ -46,3 +46,12 @@ while True:
 
     tShift += tPause
 
+# myController = Controller()
+# myController.send(None)
+#
+# while True:
+#     myCntrl = list(myController)
+#     print (myController)
+#     print(myCntrl)
+#     for i in myCntrl:
+#         print("%.5f\t%d\t%d\t%d" % myCntrl[0], myCntrl[1], myCntrl[2], myCntrl[3])
